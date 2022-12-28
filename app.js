1541ad517f957fca00e5078a44bdeb2ffbbf6780
Saizalsinha <saizalsinha2003@gmail.com>
2022-12-28 19:10:52 +0530

    Add about and contact page

--- app.js
+++ app.js
@@ -19,9 +19,13 @@ app.get("/",function(req,res){
   res.render('home',{startingContent:homeStartingContent});
 });
 
+app.get("/about",function(req,res){
+  res.render("about",{content:aboutContent});
+});
 
-
-
+app.get("/contact",function(req,res){
+  res.render("contact",{content:contactContent});
+});
 
 
 
